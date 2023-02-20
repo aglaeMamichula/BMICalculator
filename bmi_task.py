8066bc638bd4b3f53cd425413f0e792eaacd17b2
aglaeMamichula <125798530+aglaeMamichula@users.noreply.github.com>
2023-02-20 20:44:42 +0000

    Add files via upload

---
+++ bmi_task.py
@@ -0,0 +1,33 @@
+#TASK2
+#Ask the user to enter their weight in kg and their height in m
+#Use the formula below to calculate the user's BMI:BMI = (weight in kg) / ((height in m)*(height in m))
+#If the user’s BMI is 30 or greater the user is obese
+#If the user’s BMI is 25 or greater the user is overweight
+#If the user’s BMI is 18.5 or greater the user is normal
+#If the user’s BMI is less than 18.5 the user is underweight
+#Display the user’s BMI and whether they are obese, overweight, normal or underweight.
+
+weight = float(input("please enter your weight in kg"))
+height = float(input("please enter your height in m"))
+
+BMI = (weight)/(height*height)
+#just change the formula as mmy tutor correct me, the formula was wrong it make sense becaosue it told em i was obesse lol
+print (BMI)
+
+if BMI >= 30:
+    print ("you are obesse")
+    
+
+elif BMI >25:
+    print("you are overweight")
+    
+    
+    
+elif BMI >= 18.5:
+    print("you are normal")
+    
+    
+   
+else:
+    print("you are underweight")
+
